52ac2d6f6e15c5bf983f26d8475e2d0947419b02
Vishal <vishal@openspaceservices.com>
2022-06-09 11:37:29 +0530

    marshmallow validation added

--- schemas/requestCompanySchema.py
+++ schemas/requestCompanySchema.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
-from marshmallow import Schema,fields
+from marshmallow import Schema,fields,validate
 
 class company_pydantic(BaseModel):
     company_name: Optional[str]
@@ -13,11 +13,11 @@ class company_pydantic(BaseModel):
     address: Optional[str]
 
 class company_marsh(Schema):
-    company_name = fields.String(required = True)
-    country= fields.String(required = True)
-    state= fields.String(required = True)
-    city= fields.String(required = True)
-    pincode= fields.String(required = True)
-    department= fields.String(required = True)
-    branch= fields.String(required = True)
-    address= fields.String(required = True)
+    company_name = fields.String(required = True,validate=validate.Length(min=3))
+    country = fields.String(required = True,validate=validate.Length(min=5))
+    state = fields.String(required = True,validate=validate.Length(min=3))
+    city = fields.String(required = True,validate=validate.Length(min=3))
+    pincode = fields.String(required = True,validate=[validate.Length(min=6,max=6),validate.Regexp(r'[0-9]',error='Please Enter Valid Pincode')])
+    department = fields.String(required = True,validate=validate.Length(min=3))
+    branch = fields.String(required = True,validate=validate.Length(min=3))
+    address = fields.String(required = True,validate=validate.Length(min=10))
--- schemas/requestUserSchema.py
+++ schemas/requestUserSchema.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from marshmallow import Schema,fields
+from marshmallow import Schema,fields,validate
 from pydantic import BaseModel
 
 class userPydantic(BaseModel):
@@ -23,19 +23,19 @@ class update_userPydantic(BaseModel):
 
 class user_marsh(Schema):
     c_id = fields.Integer(required = True)
-    fullname = fields.String(required = True)
+    fullname = fields.String(required = True,validate=[validate.Regexp(regex=r'^[a-zA-Z ]+$',error="Please enter a valid fullname"),validate.Length(min=5)])
     email = fields.Email(required = True)
-    password = fields.String(required = True)
-    contact_no = fields.String(required = True)
+    password = fields.String(required = True,validate=validate.Length(min=8))
+    contact_no = fields.String(required = True,validate=[validate.Length(min=10),validate.Regexp(r"^[0-9]*$",error = 'PLease Enter 10 digits Conatct no.')])
     working_under = fields.Integer(required = True)
     dob = fields.Date(required = True)
     role_id = fields.Integer(required = True)
 
 class update_user_marsh(Schema):
     c_id = fields.Integer(required = True)
-    fullname = fields.String(required = True)
+    fullname = fields.String(required = True,validate=[validate.Regexp(regex=r'^[a-zA-Z ]+$',error="Please enter a valid fullname"),validate.Length(min=5)])
     email = fields.Email(required = True)
-    contact_no = fields.String(required = True)
+    contact_no = fields.String(required = True,validate=[validate.Length(min=10),validate.Regexp(r"^[0-9]*$",error = 'PLease Enter 10 digits Conatct no.')])
     working_under = fields.Integer(required = True)
     dob = fields.Date(required = True)
     role_id = fields.Integer(required = True)
